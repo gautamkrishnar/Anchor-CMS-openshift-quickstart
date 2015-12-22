13f863467a9fbd1b444ac6e44b97b58570d1360c
Gautam krishna.R <rgautamkrishna@gmail.com>
2015-12-23 04:15:37 +0530

    Update complete.php

--- php/install/views/complete.php
+++ php/install/views/complete.php
@@ -2,11 +2,13 @@
 
 <section class="content small">
 	<h1>Install complete!</h1>
-	<p class="code">This Quickstart is hosted on github. Please star it and spread the word.</p>
-	<a href="https://github.com/gautamkrishnar/Anchor-CMS-openshift-quickstart" target="_blank">https://github.com/gautamkrishnar/Anchor-CMS-openshift-quickstart</a>
+
+	<!--?php if($htaccess): ?-->
+	<p class="code">This Quickstart is hosted on github. Please star it and spread the word:</p>
+
 	<section class="options">
 		<a href="<?php echo $admin_uri; ?>" class="button">Visit your admin panel</a>
-		<a href="<?php echo $site_uri; ?>" class="right">Visit your new site</a>
+		<a href="https://github.com/gautamkrishnar/Anchor-CMS-openshift-quickstart" target="_blank" class="right">Star it on GitHub</a>
 	</section>
 </section>
 
